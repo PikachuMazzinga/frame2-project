a73846b65224a014913b653caf16b5e084876834
Marco Carraro <Marko97.PM@hotmail.it>
2025-08-15 04:26:30 +0200

    Summary UI egg fix

--- Plugins/Script Examples/v20/006_UI_Summary.rb
+++ Plugins/Script Examples/v20/006_UI_Summary.rb
@@ -912,7 +912,7 @@ class PokemonSummary_Scene
     @pokemon.play_cry
     
     # FRAME2 PROJECT ADDITION
-    @sprites["pokemon"]&.pbPlayIntroAnimation
+    @sprites["pokemon"]&.pbPlayIntroAnimation if !@pokemon.egg?
 
   end
 
@@ -1263,7 +1263,7 @@ class PokemonSummary_Scene
     @pokemon.play_cry
 
     # FRAME2 PROJECT ADDITION
-    @sprites["pokemon"]&.pbPlayIntroAnimation
+    @sprites["pokemon"]&.pbPlayIntroAnimation if !@pokemon.egg?
 
     loop do
       Graphics.update
@@ -1275,7 +1275,7 @@ class PokemonSummary_Scene
         @pokemon.play_cry
 
         # FRAME2 PROJECT ADDITION
-        @sprites["pokemon"]&.pbPlayIntroAnimation
+        @sprites["pokemon"]&.pbPlayIntroAnimation if !@pokemon.egg?
 
       elsif Input.trigger?(Input::BACK)
         pbPlayCloseMenuSE
