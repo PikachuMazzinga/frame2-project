c39862337aa3f8f98e9e7cea7e52950a56fade11
Marco Carraro <Marko97.PM@hotmail.it>
2025-11-18 18:33:22 +0100

    species symbol hotfix

--- Plugins/F2P_Gen4BattleEntryAnimations/MethodOverrides.rb
+++ Plugins/F2P_Gen4BattleEntryAnimations/MethodOverrides.rb
@@ -257,7 +257,7 @@ class Battle::Scene::BattlerSprite
 
     # Play Intro animation
     if PokemonIntroAnimationSettings::ENABLED_IN_BATTLE && PokemonIntroAnimationSettings::DEFAULT_BEHAVIOUR != nil
-      @battleAnimations.push(PokemonIntroAnimation.new([self],@viewport,@pkmn.species,@index%2 == 0))
+      @battleAnimations.push(PokemonIntroAnimation.new([self],@viewport,@pkmn,@index%2 == 0))
     end
   end
 
